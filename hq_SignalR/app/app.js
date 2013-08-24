c709130571e7eb2137d6ce303fc84a7e7a86b212
Christian Weyer <christian.weyer@thinktecture.com>
2013-08-24 15:52:22 +0200

    Encapsulating hub proxies in factories

--- hq_SignalR/app/app.js
+++
@@ -1,3 +0,0 @@
-﻿'use strict';
-
-var app = angular.module('signalRIntegrationApp', ['ngSignalR']);
---
+++ ngSignalR/app/controllers.js
@@ -0,0 +1,25 @@
+﻿app.controller("ServerTimeController", function ($scope, clientPushHubService, serverTimeHubService) {
+    clientPushHubService.on('serverTime', function (data) {
+        $scope.currentServerTime = data;
+    });
+    
+    serverTimeHubService.start().done(function () {
+        getTimeFromServer();
+    });
+    
+    $scope.getServerTime = function () {
+        getTimeFromServer();
+    };
+
+    function getTimeFromServer() {
+        serverTimeHubService.invoke('getServerTime', function (data) {
+            $scope.currentServerTimeManually = data;
+        });
+    }
+});
+
+app.controller("PerformanceDataController", function ($scope, performanceDataHubService) {
+    performanceDataHubService.on('newCpuDataValue', function (data) {
+        $scope.cpuData = data;
+    });
+});
